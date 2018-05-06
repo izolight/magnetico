e0b2425ba0aff449e43e3599bca917df58176036
Gabor Tanz <izolight@gmail.com>
2018-05-06 19:30:05 +0200

    split into separate tests

--- pkg/persistence/sqlite3_test.go
+++ pkg/persistence/sqlite3_test.go
@@ -17,32 +17,52 @@ const (
 )
 
 func TestSqlite3Database_AddNewTorrent(t *testing.T) {
+	tearDown, db := setupTest(t)
+	defer tearDown(t)
+
+	addTorrent(db, t)
+}
+
+func TestSqlite3Database_DoesTorrentExist(t *testing.T) {
 	infoHash, err := hex.DecodeString(HASH)
-	checkErr(err, t)
-	var files []File
-	file := File{
-		Path: NAME,
-		Size: SIZE,
-	}
-	files = append(files, file)
 
 	tearDown, db := setupTest(t)
 	defer tearDown(t)
 
-	err = db.AddNewTorrent(infoHash, NAME, files)
-	checkErr(err, t)
-
 	exist, err := db.DoesTorrentExist(infoHash)
+	checkErr(err, t)
+	if exist {
+		t.Fatal("expected torrent to not exist")
+	}
+
+	addTorrent(db, t)
+	exist, err = db.DoesTorrentExist(infoHash)
 	checkErr(err, t)
 	if !exist {
 		t.Fatal("expected torrent to exist")
 	}
+}
+
+func TestSqlite3Database_GetNumberOfTorrents(t *testing.T) {
+	tearDown, db := setupTest(t)
+	defer tearDown(t)
+
+	addTorrent(db, t)
 
 	n, err := db.GetNumberOfTorrents()
 	checkErr(err, t)
 	if n != 1 {
 		t.Fatal("expected there to be 1 torrent")
 	}
+}
+
+func TestSqlite3Database_GetTorrent(t *testing.T) {
+	infoHash, err := hex.DecodeString(HASH)
+
+	tearDown, db := setupTest(t)
+	defer tearDown(t)
+
+	addTorrent(db, t)
 
 	metadata, err := db.GetTorrent(infoHash)
 	checkErr(err, t)
@@ -63,9 +83,20 @@ func TestSqlite3Database_AddNewTorrent(t *testing.T) {
 	if metadata.DiscoveredOn > now {
 		t.Fatalf("Timestamp is in the future. Now: %v, Got: %v", now, metadata.DiscoveredOn)
 	}
+}
+
+func TestSqlite3Database_GetFiles(t *testing.T) {
+	infoHash, err := hex.DecodeString(HASH)
+
+	tearDown, db := setupTest(t)
+	defer tearDown(t)
+
+	addTorrent(db, t)
 
 	tFiles, err := db.GetFiles(infoHash)
 	checkErr(err, t)
+
+	t.Log(tFiles)
 
 	if len(tFiles) != 1 {
 		t.Fatalf("filenumber mismatch. Expected: 1, Got: %d", len(tFiles))
@@ -80,14 +111,27 @@ func TestSqlite3Database_AddNewTorrent(t *testing.T) {
 	}
 }
 
-func TestSqlite3Database_DoesTorrentExist(t *testing.T) {
+func TestSqlite3Database_GetStatistics(t *testing.T) {
 	tearDown, db := setupTest(t)
 	defer tearDown(t)
-	infoHash, err := hex.DecodeString("e4be9e4db876e3e3179778b03e906297be5c8dbe")
-	exist, err := db.DoesTorrentExist(infoHash)
+
+	addTorrent(db, t)
+
+	dataPoints := uint(2)
+
+	stats, err := db.GetStatistics(dataPoints, "2018")
 	checkErr(err, t)
-	if exist {
-		t.Fatal("torrent should not exist")
+
+	if stats.N != dataPoints {
+		t.Fatalf("expected there to be %d datapoints, got %d", dataPoints, stats.N)
+	}
+
+	if stats.NFiles[0] != 0 && stats.TotalSize[0] != 0 && stats.NTorrents[0] != 0 {
+		t.Fatalf("expected there to be no torrents in previous year")
+	}
+
+	if stats.NFiles[1] != 1 && stats.TotalSize[1] != SIZE && stats.NTorrents[1] != 1 {
+		t.Fatalf("expected there to be our torrent")
 	}
 }
 
@@ -105,6 +149,8 @@ func setupTest(t *testing.T) (func(t *testing.T), Database) {
 
 	dbUrl, err := url.Parse("sqlite3:///tmp/test.db")
 	checkErr(err, t)
+	// ensure file does not exist before
+	os.Remove(dbUrl.Path)
 
 	db, err := MakeDatabase(dbUrl, logger)
 	checkErr(err, t)
@@ -113,6 +159,20 @@ func setupTest(t *testing.T) (func(t *testing.T), Database) {
 		os.Remove(dbUrl.Path)
 		t.Log("teardown db")
 	}, db
+}
+
+func addTorrent(db Database, t *testing.T) {
+	infoHash, err := hex.DecodeString(HASH)
+	checkErr(err, t)
+	var files []File
+	file := File{
+		Path: NAME,
+		Size: SIZE,
+	}
+	files = append(files, file)
+
+	err = db.AddNewTorrent(infoHash, NAME, files)
+	checkErr(err, t)
 }
 
 func checkErr(err error, t *testing.T) {
