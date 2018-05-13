cfa040d40d977318833ecb010ad3377b46f48879
Gabor Tanz <izolight@gmail.com>
2018-05-13 20:37:14 +0200

    show all three graphs in stats

--- cmd/magneticow/data/static/scripts/statistics.js
+++ cmd/magneticow/data/static/scripts/statistics.js
@@ -2,7 +2,7 @@
 
 
 window.onload = function() {
-    Plotly.newPlot("discoveryRateGraph", discoveryRateData, {
+    Plotly.newPlot("torrentGraph", torrentsData, {
         title: "New Discovered Torrents Per Day in the Past 30 Days",
         xaxis: {
             title: "Days",
@@ -10,6 +10,28 @@ window.onload = function() {
         },
         yaxis: {
             title: "Amount of Torrents Discovered"
+        }
+    });
+
+    Plotly.newPlot("filesGraph", filesData, {
+        title: "New Discovered Files Per Day in the Past 30 Days",
+        xaxis: {
+            title: "Days",
+            tickformat: "%d %B"
+        },
+        yaxis: {
+            title: "Amount of Files Discovered"
+        }
+    });
+
+    Plotly.newPlot("sizeGraph", sizeData, {
+        title: "New Discovered Bytes Per Day in the Past 30 Days",
+        xaxis: {
+            title: "Days",
+            tickformat: "%d %B"
+        },
+        yaxis: {
+            title: "Amount of Bytes Discovered"
         }
     });
 };
