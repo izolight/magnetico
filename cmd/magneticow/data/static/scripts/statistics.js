ac3347d4dbc3a91357638129b29c08b7ee95df64
Gabor Tanz <izolight@gmail.com>
2018-05-13 21:19:00 +0200

    make stats graph more readable and less legen overlapping

--- cmd/magneticow/data/static/scripts/statistics.js
+++ cmd/magneticow/data/static/scripts/statistics.js
@@ -4,12 +4,15 @@ var layout = {
     title: "New Discovered Torrents Per Day in the Past 30 Days",
     xaxis: {
         title: "Days",
-        tickformat: "%d %B"
+        tickformat: "%d %B",
+        domain: [0.1, 0.95]
     },
     yaxis: {
         title: "Torrents",
         titlefont: {color: '#1f77b4'},
-        tickfont: {color: '#1f77b4'}
+        tickfont: {color: '#1f77b4'},
+        side: "left",
+        position: 0
     },
     yaxis2: {
         title: "Files",
@@ -18,7 +21,7 @@ var layout = {
         anchor: 'free',
         overlaying: 'y',
         side: 'left',
-        position: 0.15
+        position: 0.07
     },
     yaxis3: {
         title: "Bytes",
@@ -27,7 +30,7 @@ var layout = {
         anchor: 'x',
         overlaying: 'y',
         side: 'right',
-        position: 0.85,
+        position: 0.95,
         tickformat: ".3s"
     }
 }
